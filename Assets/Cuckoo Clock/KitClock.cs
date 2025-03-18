816cb7e237e56c162005496e420889050f4c7576
baogr <baogr@OA-S141-12>
2025-03-18 16:25:07 -0400

    make clock stoppable

--- Assets/Cuckoo Clock/KitClock.cs
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -5,6 +5,8 @@ using UnityEngine.Events;
 
 public class KitClock : MonoBehaviour
 {
+    public Transform hourHand, minuteHand;
+
     public float timeAnHourTakes = 5;
 
     public float t;
@@ -12,20 +14,51 @@ public class KitClock : MonoBehaviour
 
     public UnityEvent OnTheHour;
 
+    Coroutine clockIsRunning;
+    IEnumerator doOneHour;
+
+    void Start()
+    {
+        clockIsRunning = StartCoroutine(MoveClock());
+    }
+
     void Update()
     {
-        t += Time.deltaTime;
+    }
 
-        if (t > timeAnHourTakes)
+    private IEnumerator MoveClock()
+    {
+        while (true)
         {
-            t = 0;
-            OnTheHour.Invoke();
+            doOneHour = MoveClockHandsOneHour();
+            yield return StartCoroutine(doOneHour);
 
-            hour++;
-            if (hour == 12)
-            {
-                hour = 0;
-            }
+            //yield return doOneHour=StartCoroutine(MoveClockHandsOneHour); if doOneHour is a coroutine type
+        }
+    }
+
+    private IEnumerator MoveClockHandsOneHour()
+    {
+        t = 0;
+        while (t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+            minuteHand.Rotate(0, 0, -(360 / timeAnHourTakes) * Time.deltaTime);
+            hourHand.Rotate(0, 0, -(30 / timeAnHourTakes) * Time.deltaTime);
+            yield return null;
+        }
+        OnTheHour.Invoke();
+    }
+
+    public void StopClock()
+    {
+        if (clockIsRunning != null)
+        {
+            StopCoroutine(clockIsRunning);
+        }
+        if (doOneHour != null)
+        {
+            StopCoroutine(doOneHour);
         }
     }
 }
