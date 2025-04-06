34f22d55d56a77fa7b73e70e26037876f1aee317
MICHAELSLAPTOP\helle <hellenhermen@gmail.com>
2025-04-06 14:07:55 -0400

    Created a way for the items to be added to the inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    // List to hold items in the inventory
+    public List<Item> items = new List<Item>();
+
+    // Maximum capacity of the inventory
+    public int maxCapacity = 10;
+
+    // Method to add an item to the inventory
+    public bool AddItem(Item item)
+    {
+        if (items.Count < maxCapacity)
+        {
+            items.Add(item);
+            Debug.Log(item.itemName + " added to inventory.");
+            return true;
+        }
+        else
+        {
+            Debug.Log("Inventory is full.");
+            return false;
+        }
+    }
+
+    // Method to remove an item from the inventory
+    public bool RemoveItem(Item item)
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            Debug.Log(item.itemName + " removed from inventory.");
+            return true;
+        }
+        else
+        {
+            Debug.Log(item.itemName + " not found in inventory.");
+            return false;
+        }
+    }
+
+    // Method to list all items in the inventory
+    public void ListItems()
+    {
+        if (items.Count == 0)
+        {
+            Debug.Log("Inventory is empty.");
+            return;
+        }
+
+        Debug.Log("Inventory Items:");
+        foreach (var item in items)
+        {
+            Debug.Log("Item: " + item.itemName + " - " + item.itemDescription);
+        }
+    }
+
+    // Method to use an item from the inventory (calls item's Use method)
+    public void UseItem(Item item)
+    {
+        if (items.Contains(item))
+        {
+            item.Use();  // Call the Use method of the item
+        }
+        else
+        {
+            Debug.Log(item.itemName + " not in inventory.");
+        }
+    }
+}
---
+++ Assets/Scripts/RandomItemSpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItemSpawner : MonoBehaviour
+{
+    public Inventory inventory; // Reference to the inventory
+    public float minInterval = 2f; // Minimum time interval (in seconds)
+    public float maxInterval = 5f; // Maximum time interval (in seconds)
+
+    // List of items to choose from
+    public Item[] possibleItems;
+
+    private void Start()
+    {
+        // Start the process of adding items at random intervals
+        StartCoroutine(AddItemAtRandomIntervals());
+    }
+
+    private IEnumerator AddItemAtRandomIntervals()
+    {
+        while (true)
+        {
+            // Wait for a random time interval
+            float interval = Random.Range(minInterval, maxInterval);
+            yield return new WaitForSeconds(interval);
+
+            // Choose a random item from the list
+            Item itemToAdd = possibleItems[Random.Range(0, possibleItems.Length)];
+
+            // Attempt to add the item to the inventory
+            if (inventory.AddItem(itemToAdd))
+            {
+                Debug.Log(itemToAdd.itemName + " added to inventory.");
+            }
+        }
+    }
+}
