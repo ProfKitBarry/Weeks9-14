6a2338ec8db63e940bccecd0225ce9db8f7286a7
ACHILLESLAPTOP\tomas <tomasiac@sheridancollege.ca>
2026-03-13 23:16:11 -0400

    coding gym session 1 30 min

--- Assets/Script/PlayerInputCodingGym.cs
+++ Assets/Script/PlayerInputCodingGym.cs
@@ -8,6 +8,7 @@ public class PlayerInputCodingGym : MonoBehaviour
     public Vector2 directionalInput;
     public Vector2 lookVector;
     public float speed = 1;
+    public GameObject barrel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,8 +18,21 @@ public class PlayerInputCodingGym : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = (Vector3)directionalInput * speed * Time.deltaTime;
+       
+        transform.position += (Vector3)directionalInput * speed * Time.deltaTime;
+      
+        rotateWithTranUp();
 
+    }
+     void rotateWithDir(){
+         // using rotation
+       /* Vector3 rot = barrel.transform.eulerAngles;
+        rot.z = lookVector.x * -360;
+        Debug.Log(rot.z);
+        barrel.transform.eulerAngles = rot;*/
+    }
+    void rotateWithTranUp(){
+        barrel.transform.up = (Vector3)lookVector.normalized;
     }
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -26,6 +40,6 @@ public class PlayerInputCodingGym : MonoBehaviour
     }
     public void Onlook(InputAction.CallbackContext context)
     {
-
+        lookVector = context.ReadValue<Vector2>();
     }
 }
