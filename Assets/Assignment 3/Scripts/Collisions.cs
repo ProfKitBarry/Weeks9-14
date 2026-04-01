8336cd2e2c2b21167477290db3aa91f6ac2fd180
saporito <saporito@OA-S213-10>
2026-03-31 23:46:02 -0400

    Worked on collisions more. I got the collision to trigger, but it will instantly trigger when I start.

--- Assets/Assignment 3/Scripts/Collisions.cs
+++ Assets/Assignment 3/Scripts/Collisions.cs
@@ -3,19 +3,27 @@ using UnityEngine;
 public class Collisions : MonoBehaviour
 {
     public SpriteRenderer enemyA;
-    public SpriteRenderer missileRenderer;
+    public EnemyPrefabs enemyAScript;
+
+    public SpriteRenderer missile;
+    public Missile missileScript;
+
+    public bool isEnemyAHit;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        missileRenderer = GetComponent<SpriteRenderer>();
+        missile = GetComponent<SpriteRenderer>();
+        
+        isEnemyAHit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isEnemyAHit = missileRenderer.bounds.Contains(enemyA.transform.position);
-
-        if (isEnemyAHit)
+        isEnemyAHit = missile.bounds.Contains(enemyAScript.transform.position);
+        
+        if (isEnemyAHit == true)
         {
             Debug.Log("Hit");
 
