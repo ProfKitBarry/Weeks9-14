e0a2e3298a9d192bf538f66b06ffabad0f18d342
Leo Saporito <saporito@shernet.sheridancollege.ca>
2026-04-04 13:17:56 -0400

    Starting next session for the programming assignment now. I'm going to ignore collisions and start coroutines.

--- Assets/Assignment 3/Scripts/Enemies.cs
+++ Assets/Assignment 3/Scripts/Enemies.cs
@@ -5,22 +5,54 @@ using UnityEngine;
 public class Enemies : MonoBehaviour
 {
     //ENEMY A
-    //public List<GameObject> enemyAList = new List<GameObject>();
+    public List<GameObject> enemyAList = new List<GameObject>();
     public GameObject enemyAPrefab;
+    private GameObject newEnemyA;
+
     public Vector3 enemyASpawnPosition;
     public int enemyASpawnDistance;
 
     //All Enemies
     public bool isSpawned;
 
+    public Coroutine FirstEnemyAUpdate;
+
     void Start()
     {
         isSpawned = true;
-        GameObject newEnemyA = Instantiate(enemyAPrefab, enemyASpawnPosition, Quaternion.identity);
+        //newEnemyA = Instantiate(enemyAPrefab, enemyASpawnPosition += new Vector3(enemyASpawnDistance, 0, 0), Quaternion.identity);
+        
+        //GameObject newEnemyA1 = Instantiate(enemyAPrefab, enemyASpawnPosition, Quaternion.identity);
+        //GameObject newEnemyA2 = Instantiate(enemyAPrefab, enemyASpawnPosition, Quaternion.identity);
+        //GameObject newEnemyA3 = Instantiate(enemyAPrefab, enemyASpawnPosition, Quaternion.identity);
+        //GameObject newEnemyA4 = Instantiate(enemyAPrefab, enemyASpawnPosition, Quaternion.identity);
+        //GameObject newEnemyA5 = Instantiate(enemyAPrefab, enemyASpawnPosition, Quaternion.identity);
     }
 
     void Update()
     {
+        if (enemyAList.Count < 5)
+        {
+            isSpawned = true;
+
+            if (isSpawned == true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    newEnemyA = Instantiate(enemyAPrefab, enemyASpawnPosition += new Vector3(enemyASpawnDistance, 0, 0), Quaternion.identity);
+                    enemyAList.Add(newEnemyA);
+                }
+
+
+                isSpawned = false;
+            }
+        }
+    }
+
+    public IEnumerator FirstEnemyA()
+    {
+
+        yield return null;
     }
 }
 //if (enemyAList.Count < 5)
